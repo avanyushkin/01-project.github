df7f898f2c3b821dbb7a5faf15769fd21c81fe7e
avanyushkin <a.vanyushkin1@gmail.com>
2025-06-28 14:23:35 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const images = document.querySelectorAll('.image'); /*получаем каждый из слайдов в строковом формате*/
+
+images[0].classList.add('active');
+
+for (const image of images) {
+    image.addEventListener('click', () => {
+        /*при клике нужно удалять класс active у всех слайдов кроме текущего*/
+        clearActiveClasses();
+        /*при клике нужно добавить класс active*/
+        image.classList.add('active')
+    })
+}
+
+function clearActiveClasses() {
+    for (const image of images) {
+        image.classList.remove('active');
+    } 
+}
